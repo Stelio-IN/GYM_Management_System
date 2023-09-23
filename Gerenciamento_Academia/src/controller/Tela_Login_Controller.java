886cb77a63a61355c8c6e44137e42ac61596ed7c
StelioAMIN <stelioinvolution@gmail.com>
2023-09-24 13:04:55 +1400

    Revert "Tela admin menu inicio"
    This reverts commit 9ad8f71957cfac4a51fc3912e95ac85ddefabde1.

--- Gerenciamento_Academia/src/controller/Tela_Login_Controller.java
+++ Gerenciamento_Academia/src/controller/Tela_Login_Controller.java
@@ -4,22 +4,9 @@
  */
 package controller;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import javax.swing.JOptionPane;
-import model.Administrador;
 
 
 /**
@@ -29,44 +16,9 @@ import model.Administrador;
  */
 public class Tela_Login_Controller implements Initializable {
 
-    @FXML
-    private TextField txtEmail;
-
-    @FXML
-    private TextField txtPassword;
-    @FXML
-    public void Tela_Principal(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Tela_Menu_Admin.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    @FXML
-    void btnLogar(ActionEvent event) {
-        GenericDAO bb = new GenericDAO();
-        Administrador aa = (Administrador) bb.logarEmail(txtEmail.getText());
-
-        if (aa != null && aa.getPassword().equals(txtPassword.getText())) {
-            try {
-                Alert alerta = new Alert(Alert.AlertType.INFORMATION);
-                alerta.setTitle("Login");
-                alerta.setHeaderText("Login efetuado com sucesso!!!!");
-                // alerta.setContentText("Deseja salvar antes de Fechar");
-                if (alerta.showAndWait().get() == ButtonType.OK) {
-
-                    Tela_Principal(event);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace(); // Trate ou registre erros adequadamente
-            }
-        } else {
-            JOptionPane.showMessageDialog(null,"Credencias erradas");
-         
-        }
-    }
+    /**
+     * Initializes the controller class.
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
