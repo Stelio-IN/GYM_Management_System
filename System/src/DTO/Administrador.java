0b3826e742f158e968a088745a3d14ee1a44e52f
StelioAMIN <stelioinvolution@gmail.com>
2023-09-24 13:04:49 +1400

    Revert "Merge branch 'main' of https://github.com/Stelio-IN/GYM_Management_System"
    This reverts commit 55bf047a106d7320ae7b7797e45ee02f7e5c636a, reversing
    changes made to 764162dc64b78619f647ed93bac325e8ac57d6c1.

---
+++ System/src/DTO/Administrador.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ *
+ * @author steli
+ */
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "Administrador.findByEmail", query = "SELECT a FROM Administrador a WHERE a.email = :email"),
+})
+public class Administrador implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String password;
+
+    
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
---
+++ System/src/View/Programa_Principal.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+package View;
+
+import DAO_Generic.GenericDAO;
+import DTO.Administrador;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author steli
+ */
+public class Programa_Principal extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Tela_Login.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("TelaPrincipal.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show();
+        stage.setOnCloseRequest(event -> {
+            event.consume();
+            close(stage);
+        });
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        Administrador admin = new Administrador();
+//        admin.setNome("gymm");
+//        admin.setEmail("gymm@gmail.com");
+//        admin.setPassword("123456");
+//        GenericDAO obj = new GenericDAO();
+//        obj.add(admin);
+        
+        launch(args);
+    }
+
+    void close(Stage stage) {
+        Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+        alerta.setTitle("FECHAR");
+        alerta.setHeaderText("Quer realment Fechar");
+        alerta.setContentText("Deseja salvar antes de Fechar");
+        if (alerta.showAndWait().get() == ButtonType.OK) {
+
+            System.out.println("Exit exito");
+            System.out.println("bla bla bla");
+            System.out.println("De volta ksksk");
+            stage.close();
+        }
+    }
+
+}
