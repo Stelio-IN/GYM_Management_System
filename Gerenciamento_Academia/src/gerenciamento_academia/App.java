11a2938da538f8f41b03449864b9e5fd77c78883
StelioAMIN <stelioinvolution@gmail.com>
2023-09-24 13:35:54 +1400

    bug resolvido

--- Gerenciamento_Academia/src/gerenciamento_academia/App.java
+++ Gerenciamento_Academia/src/gerenciamento_academia/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/view/Tela_Menu_Admin.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Tela_Login.fxml"));
         
         Scene scene = new Scene(root);
         
