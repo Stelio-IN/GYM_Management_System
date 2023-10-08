c80b351c6cb2676249bae4982a345f41c48fa21a
StelioAMIN <stelioinvolution@gmail.com>
2023-10-09 08:14:09 +1400

    Ajuste

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-09T08:08:24", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-09T08:12:40", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Funcionario.class)
 public class Funcionario_ extends Pessoa_ {
 
