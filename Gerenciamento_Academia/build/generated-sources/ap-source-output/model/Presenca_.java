9be2fcb6f721a989d5247481bd10e7c2901dda03
StelioAMIN <stelioinvolution@gmail.com>
2023-10-11 15:34:15 +1400

    a

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Presenca_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Presenca_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Cliente;
 import model.Funcionario;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-09T08:12:40", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-11T15:27:43", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Presenca.class)
 public class Presenca_ { 
 
