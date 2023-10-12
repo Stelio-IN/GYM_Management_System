a8a6bdb067bf1af8e2edba80c3c05fbf2a50d57a
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2023-10-12 21:05:40 +0200

    Tela pacotes 100%
    Tela pacotes tudo funcional

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Administrador_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Administrador_.java
@@ -3,7 +3,7 @@ package model;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-11T23:18:30", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-12T20:38:41", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Administrador.class)
 public class Administrador_ extends Pessoa_ {
 
--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Cliente_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Cliente_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.Plano_de_Associacao;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-11T23:18:30", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-12T20:38:41", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ extends Pessoa_ {
 
