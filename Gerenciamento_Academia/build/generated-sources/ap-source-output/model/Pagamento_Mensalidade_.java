18cb5a0f07d1e297b8262a83550fa2b0d305cd40
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2023-10-12 22:15:53 +0200

    Nova 1

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Cliente;
 import model.Funcionario;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-12T20:38:41", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-12T22:10:24", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Pagamento_Mensalidade.class)
 public class Pagamento_Mensalidade_ { 
 
