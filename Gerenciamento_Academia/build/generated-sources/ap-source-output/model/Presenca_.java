135c443a8b3e29c86a347ba022ba8cab40fdb849
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2023-10-20 02:55:55 +0200

    yyy

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Presenca_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Presenca_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.StaticMetamodel;
 import model.Cliente;
 import model.Funcionario;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-20T01:19:55", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-20T02:18:51", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Presenca.class)
 public class Presenca_ { 
 
--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Relatorios_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Relatorios_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.Tipo_Relatorio;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-20T01:19:55", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-20T02:18:51", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Relatorios.class)
 public class Relatorios_ { 
 
