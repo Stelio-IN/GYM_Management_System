e7695ca789225e7c81e40ce76e35e2faf751052a
StelioAMIN <stelioinvolution@gmail.com>
2023-10-23 07:44:04 +1400

    Merge sumeid&Stelio

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Cliente_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Cliente_.java
@@ -8,7 +8,7 @@ import model.Avaliacoes_Fisicas;
 import model.Cliente;
 import model.Plano_de_Associacao;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-23T05:42:11", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-23T07:40:12", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ extends Pessoa_ {
 
