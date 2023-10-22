ac9578fc2be27b8550be7a9263a19d34ccb6d8a9
StelioAMIN <stelioinvolution@gmail.com>
2023-10-23 10:01:34 +1400

    Tentando subir tela

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.Avaliacoes_Fisicas;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-23T05:42:12", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-23T09:48:09", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Funcionario.class)
 public class Funcionario_ extends Pessoa_ {
 
