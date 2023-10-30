cb6b4778878b1b95245f8337d32c9dc2d33fcd89
Sumeid Ibr <115725822+sumeidibr@users.noreply.github.com>
2023-10-30 05:53:53 +0200

    pagamento 100%

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Instrutor_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Instrutor_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.Avaliacoes_Fisicas;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-30T04:00:05", comments="EclipseLink-2.7.10.v20211216-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-30T05:51:01", comments="EclipseLink-2.7.10.v20211216-rNA")
 @StaticMetamodel(Instrutor.class)
 public class Instrutor_ extends Pessoa_ {
 
