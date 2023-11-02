8311d18dfecfd23d7c32b1a0a4d63ad2b4d03feb
StelioAMIN <stelioinvolution@gmail.com>
2023-11-03 03:14:16 +1400

    add

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Administrador_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Administrador_.java
@@ -3,7 +3,7 @@ package model;
 import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-03T02:53:43", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-03T03:10:49", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Administrador.class)
 public class Administrador_ extends Pessoa_ {
 
