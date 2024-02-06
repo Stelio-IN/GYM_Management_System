bcefde3c141d58bbebd2164fea2bc7cdda40cc86
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-02-06 21:40:03 +0200

    add

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Funcionario_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.Avaliacoes_Fisicas;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-05T21:30:46", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-06T21:36:22", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Funcionario.class)
 public class Funcionario_ extends Pessoa_ {
 
--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.PlanoCliente;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-05T21:30:46", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-06T21:36:22", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Pagamento_Mensalidade.class)
 public class Pagamento_Mensalidade_ { 
 
