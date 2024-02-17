2d1e2c091befa05b763ff8755b45a8d0f535b453
Stelio Acácio Mondlane <109701079+Stelio-IN@users.noreply.github.com>
2024-02-17 08:57:06 +0200

    add

--- Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
+++ Gerenciamento_Academia/build/generated-sources/ap-source-output/model/Pagamento_Mensalidade_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import model.PlanoCliente;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-15T16:26:06", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-02-17T08:50:24", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Pagamento_Mensalidade.class)
 public class Pagamento_Mensalidade_ { 
 
